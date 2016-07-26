a93fc625e26b9e0e14fd7a036cebfc81395fc3e5
yy <woshiyiyiyeye@hotmail.com>
2016-07-25 20:24:34 -0800

    Added some commont syntax

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>\d+)$', views.post_detail, name='post_detail'),
+	url(r'^post/(?P<pk1>\d+)$', views.post_detail, name='post_detail'),
+	# There are two functions of this urlpattern, the first one is to create anurls.py file url using {%url%} model
+	# which follows the regular expression here. The second function is to match the website address to
+	# a certain veiw.
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,11 @@ from .models import Post
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+	# __lte means less than or equal, this is used when filtering the QuerySets 
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
-def post_detail(request, pk):
-	post = get_object_or_404(Post,pk=pk)
+def post_detail(request, pk1):
+	post = get_object_or_404(Post,pk=pk1)
+	# pk is a parameter which is similar to id, and server finds the post in Post according to pk value
 	return render(request, 'blog/post_detail.html', {'post':post})
 	
