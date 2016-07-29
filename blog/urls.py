cf7d5fadc88820abc90305860cffe0d00e36af7e
yy <woshiyiyiyeye@hotmail.com>
2016-07-28 22:49:07 -0800

    Added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.post_list, name='post_list'),
 	url(r'^post/(?P<pk1>\d+)$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk1>\d+)/edit/$', views.post_edit, name='post_edit'),
 	# There are two functions of this urlpattern, the first one is to create anurls.py file url using {%url%} model
 	# which follows the regular expression here. The second function is to match the website address to
 	# a certain veiw.
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
@@ -12,4 +14,37 @@ def post_detail(request, pk1):
 	post = get_object_or_404(Post,pk=pk1)
 	# pk is a parameter which is similar to id, and server finds the post in Post according to pk value
 	return render(request, 'blog/post_detail.html', {'post':post})
-	
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST) #Create a form instance with POST data.
+		if form.is_valid():
+			post = form.save(commit=False)# Create, but don't save the new instance.
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk1=post.pk)
+
+	else:
+		form = PostForm()
+
+	return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_edit(request, pk1):
+	post = get_object_or_404(Post, pk=pk1)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		# A subclass of ModelForm can accept an existing model instance as the keyword argument instance; 
+		# if this is supplied, save() will update that instance. 
+		# If it’s not supplied, save() will create a new instance of the specified model: 
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk1=post.pk)
+
+	else:
+		form = PostForm(instance=post)
+
+	return render(request, 'blog/post_edit.html', {'form':form})
